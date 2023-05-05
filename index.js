57d3dba14e3ad31802fbf45c32dc22df89e43959
geekguri-tech <geekguri1232@gmail.com>
2023-05-05 15:11:45 +0530

    Second commit with added EJS

--- index.js
+++ index.js
@@ -4,10 +4,44 @@ const express = require("express");
 
 const app = express();
 
+app.set("view engine", "ejs");
+
+
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", function(req, res){
-    res.sendFile(__dirname + "/free.html");
+
+    var today = new Date();
+    var currentDay = today.getDay();
+    var day = "";
+
+    switch(currentDay){
+        case 0:
+            day = "Sunday";
+            break;
+        case 1:
+            day = "Monday";
+            break;
+        case 2:
+            day = "Tuesday";
+            break;
+        case 3:
+            day = "Wednesday";
+            break;
+        case 4:
+            day = "Thursday";
+            break;
+        case 5:
+            day = "Friday";
+            break;
+        case 6:
+            day="Saturday";
+            break;
+        default:
+            console.log("Error");
+    }
+
+    res.render("frame", {KindOfDay: day});
 });
 
 app.listen(4000, function(){
